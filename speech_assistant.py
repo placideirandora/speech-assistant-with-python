e8b96f2b7d17f96f1aa3e8498264af21e91bc99a
Placide IRANDORA <placideirandora@gmail.com>
2019-12-23 20:41:21 +0200

    Allow user to keep asking what to do and quit by saying 'exit'

--- speech_assistant.py
+++ speech_assistant.py
@@ -1,11 +1,12 @@
 import speech_recognition as sr
-from time import ctime
+import time
 import webbrowser
 
 # Create an object
 r = sr.Recognizer()
 
-def record_speech(question = ""):
+
+def record_speech(question=""):
     # Use microphone as speech source
     with sr.Microphone() as source:
         if question:
@@ -16,19 +17,21 @@ def record_speech(question = ""):
 
         try:
             speech_text = r.recognize_google(speech)
-            print("Oh My Goodness! This Is What You Have Just Asked For:\n")
         except sr.UnknownValueError:
             print("Sorry, I Could Not Understand What You Have Just Said.\n")
         except sr.RequestError:
-            print("Sorry, I Could Not Connect To The Internet. Resolve The Issue And Come Back.")
+            print(
+                "Sorry, I Could Not Connect To The Internet. Resolve The Issue And Come Back.")
 
         return speech_text
 
 
 def respond_to_speech(speech_text):
     if "what time is it" in speech_text:
-        print(ctime())
-    
+        print("Oh My Goodness! This Is What You Have Just Asked For:\n")
+
+        print(time.ctime())
+
     if "find location" in speech_text:
         location = record_speech("What is the location?")
 
@@ -37,8 +40,15 @@ def respond_to_speech(speech_text):
 
         print("The location " + location + " has been found.")
 
+    if "exit" in speech_text:
+        exit()
+
+
+time.sleep(1)
 
 print("Tell Me What You Would Like To Do And I Will Do It For You.\n")
-speech_text = record_speech()
 
-respond_to_speech(speech_text)
+while 1:
+    speech_text = record_speech()
+
+    respond_to_speech(speech_text)
