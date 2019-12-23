bd4666bfdcf1adac94db914bbd45366ee614f43b
Placide IRANDORA <placideirandora@gmail.com>
2019-12-23 19:43:44 +0200

    Implement Code Reusability

--- speech_assistant.py
+++ speech_assistant.py
@@ -3,16 +3,24 @@ import speech_recognition as sr
 # Create an object
 r = sr.Recognizer()
 
-# Use microphone as speech source
-with sr.Microphone() as source:
-    print("Tell Me What You Want And I Will Write It Down For You.\n")
-    speech = r.listen(source)
+def record_speech():
+    # Use microphone as speech source
+    with sr.Microphone() as source:
+        speech = r.listen(source)
+        speech_text = ""
 
-    try:
-        speech_text = r.recognize_google(speech)
-        print("Oh My Goodness! This Is What You Have Just Said:\n")
-        print(speech_text)
-    except sr.UnknownValueError:
-        print("Sorry, I Could Not Understand What You Have Just Said.\n")
-    except sr.RequestError:
-        print("Sorry, I Could Not Connect To The Internet. Resolve The Issue And Come Back.")
+        try:
+            speech_text = r.recognize_google(speech)
+            print("Oh My Goodness! This Is What You Have Just Said:\n")
+        except sr.UnknownValueError:
+            print("Sorry, I Could Not Understand What You Have Just Said.\n")
+        except sr.RequestError:
+            print("Sorry, I Could Not Connect To The Internet. Resolve The Issue And Come Back.")
+
+        return speech_text
+
+
+print("Tell Me What You Want And I Will Write It Down For You.\n")
+speech_text = record_speech()
+
+print(speech_text)
