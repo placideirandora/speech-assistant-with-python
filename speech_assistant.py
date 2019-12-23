27822a3e7a7fabc44b4e22a692738f2027115ad0
Placide IRANDORA <placideirandora@gmail.com>
2019-12-23 19:33:04 +0200

    Handle the network connectivity exception and capitalize the strings

--- speech_assistant.py
+++ speech_assistant.py
@@ -5,12 +5,14 @@ r = sr.Recognizer()
 
 # Use microphone as speech source
 with sr.Microphone() as source:
-    print("Tell me what you want and I write it for you.\n")
+    print("Tell Me What You Want And I Will Write It Down For You.\n")
     speech = r.listen(source)
 
     try:
         speech_text = r.recognize_google(speech)
-        print("This is what you have said:\n")
+        print("Oh My Goodness! This Is What You Have Just Said:\n")
         print(speech_text)
     except sr.UnknownValueError:
-        print("I could not understand what you said.\n")
+        print("Sorry, I Could Not Understand What You Have Just Said.\n")
+    except sr.RequestError:
+        print("Sorry, I Could Not Connect To The Internet. Resolve The Issue And Come Back.")
