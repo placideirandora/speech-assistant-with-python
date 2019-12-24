5fa548f1b104e885299123f3164b1b170ecc2fa9
Placide IRANDORA <placideirandora@gmail.com>
2019-12-24 11:43:31 +0200

    Change file name

---
+++ text_to_speech.py
@@ -0,0 +1,14 @@
+import os
+import random
+from gtts import gTTS
+import playsound
+
+# Function for converting text speech into audio speech as well as playing it
+def speak(speech_text):
+    tts = gTTS(speech_text, lang="en")
+    random_number = random.randint(1, 1000)
+    audio_file = "speech-" + str(random_number) + ".mp3"
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(speech_text)
+    os.remove(audio_file)
